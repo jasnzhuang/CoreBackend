f41df087490967d267a3dffb6386683e2fdaa00d
Jason Zhuang <zhuangdx@gmail.com>
2018-05-18 23:27:56 +0800

    day2

---
+++ CoreBackend/Controller/ProductController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreBackend.Api.Dtos;
+using CoreBackend.Api.Services;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBackend.Api.Controllers
+{
+    //[Route("api/product")]
+    [Route("api/[controller]")]
+    public class ProductController:Controller
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(ProductService.Current.Products); //return Ok，这个Ok就等于是返回http状态200
+        }
+
+        [Route("{id}", Name = "GetProduct")]
+        public IActionResult GetProduct(int id)
+        {
+            var product = ProductService.Current.Products.SingleOrDefault(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+        //表示请求的谓词是Post. 加上Controller的Route前缀, 那么访问这个Action的地址就应该是: 'api/product'
+        // 后边也可以跟着自定义的路由地址, 例如 [HttpPost("create")], 那么这个Action的路由地址就应该是: 'api/product/create'.
+        //
+        // 请求的body里面包含着方法需要的实体数据, 方法需要把
+        // 这个数据Deserialize成ProductCreation, [FromBody]就是干这些活的.
+        [HttpPost]
+        public IActionResult Post([FromBody] ProductCreation product) 
+        {
+            if (product == null)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var maxId = ProductService.Current.Products.Max(x => x.Id);
+            var newProduct = new Product
+            {
+                Id = ++maxId,
+                Name = product.Name,
+                Price = product.Price,
+                Description = product.Description
+            };
+            ProductService.Current.Products.Add(newProduct);
+
+            return CreatedAtRoute("GetProduct", new { id = newProduct.Id }, newProduct);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] ProductModification product)
+        {
+            if (product == null)
+            {
+                return BadRequest();
+            }
+
+            if (product.Name == "产品")
+            {
+                ModelState.AddModelError("Name", "产品的名称不可以是'产品'二字");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var model = ProductService.Current.Products.SingleOrDefault(x => x.Id == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            model.Name = product.Name;
+            model.Price = product.Price;
+            model.Description = product.Description;
+            // return Ok(model);
+            return NoContent();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, [FromBody] JsonPatchDocument<ProductModification> patchDoc)
+        {
+            if (patchDoc == null)
+            {
+                return BadRequest();
+            }
+            var model = ProductService.Current.Products.SingleOrDefault(x => x.Id == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            var toPatch = new ProductModification
+            {
+                Name = model.Name,
+                Description = model.Description,
+                Price = model.Price
+            };
+            patchDoc.ApplyTo(toPatch, ModelState);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (toPatch.Name == "产品")
+            {
+                ModelState.AddModelError("Name", "产品的名称不可以是'产品'二字");
+            }
+            TryValidateModel(toPatch);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            model.Name = toPatch.Name;
+            model.Description = toPatch.Description;
+            model.Price = toPatch.Price;
+
+            return NoContent();
+        }
+    }
+}
--- CoreBackend/Startup.cs
+++ CoreBackend/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CoreBackend
@@ -15,6 +16,13 @@ namespace CoreBackend
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc() // 注册MVC到Container
+                .AddMvcOptions(options => //asp.net core 默认只实现了json.
+                                          // 为了也能够支持返回XML格式，
+                                          // 可以在ConfigureServices里面修改Mvc的配置来添加xml格式:
+            {
+                options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,10 +32,20 @@ namespace CoreBackend
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler(); //在正式环境中, 我们遇到exception的时候, 需要捕获并把它记录(log)下来
+                                           //这时候我们应该使用这个middleware: Exception Handler Middleware, 我们可以这样调用它:
+                                           // UseExceptionHandler是可以传参数的, 但暂时先这样, 我们在app.Run方法里抛一个异常
+                                           // 然后运行程序, 在Chrome里按F12就会发现有一个(或若干个, 多少次请求, 就有多少个错误)500错误.
+                                           // 用来创建 Web Api的middleware:
+            }
+            app.UseStatusCodePages(); // !!!status code middleware
+            app.UseMvc();// ConfigureServices里面写完services.AddMvc()后，在这里也要写上这条来告诉程序使用MVC
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("API源，非礼勿视!");
             });
         }
     }
